ea25c2b3c3cd7bc2f0e17d708aea878526dce87d
gevuong <gevuong18@gmail.com>
2022-11-23 14:51:32 -0800

    update subsets ii

--- backtracking/subsets_ii_bfs.js
+++ backtracking/subsets_ii_bfs.js
@@ -1,45 +1,71 @@
 /*
 https://leetcode.com/problems/subsets-ii/
 
-Runtime: 124 ms, faster than 35.00% of JavaScript online submissions for Subsets II.
-Memory Usage: 44.9 MB, less than 45.38% of JavaScript online submissions for Subsets II.
+Runtime: 107 ms, faster than 66.82% of JavaScript online submissions for Subsets II.
+Memory Usage: 44.3 MB, less than 79.56% of JavaScript online submissions for Subsets II.
 
 TC O(nlogn + n x 2^n) -> O(n x (logn + 2^n)) -> O(n x 2^n)
-SC O(logn) ?
+SC O(logn + n) to O(n + n) -> O(n) worst case.
+Depending on language used to sort, space ranges from O(logn) to O(n).
+We are not considering space required to store output, if so it would be we are making a copy of a subset of max length n.
+
+To prevent duplicates, we only want to add a duplicate number to ONLY the previously added subsets.
+For example:
+nums = [1,2,2,2]
+
+Step 1:
+add 1 to subsets: 
+[[], [1]]
+
+Step 2:
+add first 2 to subsets:
+[[], [1], [2], [1,2]]
+
+Step 3:
+add second 2 to subsets regardless of duplicates:
+[[], [1], [2], [1,2], [2], [1,2], [2,2], [1,2,2]]
+
+There are duplicates in this subset.
+Therefore, we only add second 2 to previously added subset of [2] and [1,2].
+[[], [1], [2], [1,2], [2,2], [1,2,2]]
+
+Step 4:
+Add third 2 to previously added subset of [2,2] and [1,2,2].
+[[], [1], [2], [1,2], [2,2], [1,2,2], [2,2,2], [1,2,2,2]]
 */
 var subsetsWithDup = function(nums) {
-    // sort nums in-place in ascending order.
+    // sort nums in ascending order.
     nums.sort((a, b) => a - b);
     
-    // define subsets
-    const subsets = [[]],
-          numsLen = nums.length;
-    let startIdx = 0,
-        endIdx = 0;
+    // define subsets array, start and end indices to insert 
+    const numsLen = nums.length,
+          subsets = [[]];
+    let start = 0,
+        end = 0;
     
     // loop nums
     for (let i = 0; i < numsLen; i++) {
         // reset start index
-        startIdx = 0;
+        start = 0;
         
-        // if previous and current num are equal,
-        // update start index to the previous end index.
-        if (i > 0 && nums[i] === nums[i-1]) {
-            startIdx = endIdx;
-        }
+        // for each num, check if prev and current nums are equal.
+        // if so, update start index to when the end index last ended.
+        // This is because we want to add current num ONLY to the prev set of subsets.
+        // This will prevent duplicates.
+        if (i > 0 && nums[i-1] === nums[i]) start = end;
         
-        // update end index, 
-        // and loop between start and end indices.
-        endIdx = subsets.length;        
-        for (let j = startIdx; j < endIdx; j++) {
-            // make copy of current subset
-            const subCopy = subsets[j].slice();
-            // push num to subset copy
-            subCopy.push(nums[i]);
-            // push subset copy to subsetes
-            subsets.push(subCopy);
+        // update end index to length of subsets,
+        // because we want to loop every subset.
+        end = subsets.length;
+    
+        // loop each subset from start to end indices.
+        for (let j = start; j < end; j++) {
+            // make copy of subset, push num to copy,
+            // push copy to subsets.
+            const copy = subsets[j].slice();
+            copy.push(nums[i]);
+            subsets.push(copy);
         }
     }
-    
     return subsets;
 };
