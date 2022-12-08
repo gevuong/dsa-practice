12629f44ed020c2a1c418a36dd7f465743f43eee
gevuong <gevuong18@gmail.com>
2022-12-07 16:28:00 -0800

    add min avg diff prefix sum solution

--- prefix_sum/min_avg_difference.js
+++ prefix_sum/min_avg_difference.js
@@ -19,18 +19,18 @@ var minimumAverageDifference = function(nums) {
     let totalSum = 0;
     for (const n of nums) totalSum += n;
     
-    // define left sum, left and right avgs.
-    let leftSum = 0,
-        leftAvg, rightAvg;
+    // define prefix sum, left and right avgs.
+    let prefixSum = 0,
+        prefixAvg, rightAvg;
     
     // loop nums
     for (let i = 0; i < numsLen; i++) {
         // for each element, we add to left sum and subtract from total sum.
-        leftSum += nums[i];
+        prefixSum += nums[i];
         totalSum -= nums[i];
         
         // calc avg of first i + 1 elements, rounded down.        
-        leftAvg = Math.floor(leftSum / (i+1));
+        prefixAvg = Math.floor(prefixSum / (i+1));
         
         // calc avg of last n - i - 1 elements, rounded down.
         // when we get to last index, the total sum will equal 0, 
@@ -39,9 +39,14 @@ var minimumAverageDifference = function(nums) {
         if (i < numsLen - 1) rightAvg = Math.floor(totalSum / (numsLen - i - 1));
     
         // calc abs diff between both avgs
-        const absDiff = Math.abs(leftAvg - rightAvg);
+        const absDiff = Math.abs(prefixAvg - rightAvg);
         
         // update min index and min avg diff.
+        // don't have to worry about choosing the smaller index if the min avg
+        // difference is equal because we are not considering if absDiff = minAvgDiff.
+        // Since we are iterating in ascending order, this means that the first
+        // index we encounter to have an absDiff < minAvgDiff will always be
+        // less than the subsequence indices.
         if (absDiff < minAvgDiff) {
             minIdx = i;
             minAvgDiff = absDiff;
