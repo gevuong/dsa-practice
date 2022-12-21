4efd123d0c85d3e1f88519fbc7b80c0fc1fbbe91
gevuong <gevuong18@gmail.com>
2022-12-20 20:25:50 -0600

    add find the town judge one array solution

--- p00_arrays_and_hashes/graph_related/find_the_town_judge.js
+++ p00_arrays_and_hashes/graph_related/find_the_town_judge.js
@@ -28,7 +28,7 @@ O(2N). There are 2 arrays of size N+1.
 var findJudge = function(n, trust) {
     // define in and out degrees of size n.
     // an out degree is the number of outgoing edges from a vertex,
-    // an an in-degree is the number of incomign edges to a vertex.
+    // an an in-degree is the number of incoming edges to a vertex.
     const inDegCount = Array(n+1).fill(0),
         outDegCount = Array(n+1).fill(0);
 
@@ -45,6 +45,37 @@ var findJudge = function(n, trust) {
         // if out-degree count is 0, and in-degree count is n-1,
         // return vertex
         if (outDegCount[i] === 0 && inDegCount[i] === n-1) return i;
+    }
+
+    return -1;
+};
+
+
+/*
+Method
+One Array
+*/
+
+var findJudge = function(n, trust) {
+    // define an array of degree count of size n.
+    // an out degree is the number of outgoing edges from a vertex,
+    // an an in-degree is the number of incoming edges to a vertex.
+    const degCount = Array(n+1).fill(0);
+
+    // loop edges in trust array
+    for (let [outDeg, inDeg] of trust) {
+        // increment in deg count, and decrement out deg count.
+        // out deg - in deg
+        degCount[outDeg]--;
+        degCount[inDeg]++;
+    }
+
+    // loop n vertices
+    for (let i = 1; i <= n; i++) {
+        // if degree count is n-1, return vertex.
+        // if out deg - in deg = n - 1, 
+        // then vertex is the town judge.
+        if (degCount[i] === n-1) return i;
     }
 
     return -1;
