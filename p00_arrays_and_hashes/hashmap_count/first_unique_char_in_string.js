a778a031a5706c7594da9d4a0ba3375c624ef4a2
gevuong <gevuong18@gmail.com>
2023-01-25 16:17:14 -0800

    add first unique char in string solution to hashmap_count folder

---
+++ p00_arrays_and_hashes/hashmap_count/first_unique_char_in_string.js
@@ -0,0 +1,39 @@
+/*
+# Problem
+https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+# Method
+Count (via Hashmap) + Two Pass
+
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time:
+O(2n) => O(n).
+
+- Space:
+O(26) => O(1).
+*/
+
+var firstUniqChar = function(s) {
+    // define char count
+    const chCount = {};
+
+    // fill char count
+    for (const ch of s) {
+        if (ch in chCount) chCount[ch]++;
+        else chCount[ch] = 1;
+    }
+
+    // loop s, and key each char in char count,
+    // if count equals 1, then return current index
+    for (let i = 0; i < s.length; i++) {
+        if (chCount[s[i]] === 1) return i;
+    }
+
+    return -1;
+};
